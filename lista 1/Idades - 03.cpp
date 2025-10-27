9e29ccaf981fff26b0ea83332e6f528a537a0503
joaogabrielfontinele08 <joao.fontinele@aluno.fiema.org.br>
2025-10-27 14:43:01 -0300

    Add files via upload

---
+++ lista 1/Idades - 03.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main (){
+    string nome1, nome2;
+    int idade1, idade2;
+    cout << "Dados da primeira pessoa:" << endl;
+    cout << "Nome: ";
+    getline(cin, nome1);
+    cout << "Idade: ";
+    cin >>idade1;
+    cin.ignore();
+   
+    cout << "Dados da segunda pessoa:" << endl;
+    cout << "Nome: ";
+    getline(cin, nome2);
+    cout << "Idade: ";
+    cin >> idade2;
+
+    double media = (idade1 + idade2) / 2.0;
+
+    cout << fixed << setprecision(1);
+    cout << "A idade media de " << nome1 << "e" << nome2 << "e de" << media << "anos" << endl;
+
+    return 0;
+}
